81972fd34f7223f271d951388a7dcf065b24554f
Peter Leong <peter.leong@kodak.com>
2019-08-09 12:10:05 -0700

    Polymorphism - hiding vs override

--- learn-c-sharp/fundamental-c-sharp/Polymorphism.cs
+++ learn-c-sharp/fundamental-c-sharp/Polymorphism.cs
@@ -4,20 +4,30 @@ namespace fundamental_c_sharp
     {
         class Parent
         {
-            public string Hello() => "world";
+            public string Hello() => "parent1";
+
+            public virtual string HelloV() => "parent2";
         }
 
         class Child : Parent
         {
-            public new string Hello() => "yellow";
+            public new string Hello() => "child1";
+
+            public override string HelloV() => "child2";
         }
 
         public static void Demo()
         {
             var parent = new Parent();
-            Util.WriteLn($"Expecting 'world': {parent.Hello()}");
+            Util.WriteLn($"Expecting 'parent1': {parent.Hello()}");
             var child = new Child();
-            Util.WriteLn($"Expecting 'yellow': {child.Hello()}");
+            Util.WriteLn($"Expecting 'child1': {child.Hello()}");
+            Util.WriteLn($"Expecting 'child2': {child.HelloV()}");
+            
+            // pretty whacky effect of 'new'
+            parent = child;
+            Util.WriteLn($"Expecting 'parent1' due to 'hiding' polymorphism: {parent.Hello()}");
+            Util.WriteLn($"Expecting 'child2' due to 'override' polymorphism: {parent.HelloV()}");
         }
     }
 }
