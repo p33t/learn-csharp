396a3c8533a17ea73839088f29ede80f91b45b34
Peter Leong <peter.leong@kodak.com>
2019-08-09 16:33:04 -0700

    Better way to identify target of a func.

--- learn-c-sharp/fundamental-c-sharp/Functions.cs
+++ learn-c-sharp/fundamental-c-sharp/Functions.cs
@@ -4,21 +4,20 @@ namespace fundamental_c_sharp
 {
     public class Functions
     {
-
         class Wrapper
         {
             public String meth(String s) => $"Ciao {s}";
         }
-        
+
         public static void Demo()
         {
             Func<string, string> hello = (arg) => $"Hello {arg}";
             Describe(hello);
             Func<string, string> methInst = new Wrapper().meth;
             Describe(methInst);
-            
+
             // Trying to identify a wrapper function/class
-            if(methInst.Target.GetType() == typeof(Wrapper)) Console.WriteLine("Yay!  Identified");
+            if (methInst.Target is Wrapper) Console.WriteLine("Yay!  Identified");
         }
 
         private static void Describe(Func<string, string> hello)
