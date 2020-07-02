553b6f35814fddd1cfab2250cb058df9bddfe6cb
Peter Leong <leongp@wello.ca>
2020-07-02 10:04:48 -0700

    Enum.HasFlag() testing

--- fundamental-c-sharp/Enums.cs
+++ fundamental-c-sharp/Enums.cs
@@ -13,20 +13,28 @@ namespace fundamental_c_sharp
         [Flags]
         public enum MyFlags
         {
-            None =  0b_0000,
-            Before =  0b_0001,
-            After = 0b_0010,
+            None =  0,
+            Before =  1 << 1,
+            After = 1 << 2,
+            During = 1 << 3,
         }
         public static void Demo()
         {
             Trace.Assert(MyEnum.First.ToString() == "First");
             Util.WriteLn("Enum ToString() works");
 
-            var both = MyFlags.Before | MyFlags.After;
-            Util.WriteLn($"Expecting Before, After: {both}");
+            var beforeAndAfter = MyFlags.Before | MyFlags.After;
+            Util.WriteLn($"Expecting Before, After: {beforeAndAfter}");
 
             Util.WriteLn($"Expecting no overlap (True): {(MyFlags.Before & MyFlags.After) == MyFlags.None}");
             Util.WriteLn($"Expecting overlap (False): {(MyFlags.Before & MyFlags.Before) == MyFlags.None}");
+            
+            // Looks like 'None' is always considered present
+            // Trace.Assert(!beforeAndAfter.HasFlag(MyFlags.None), "Seems like 'None' is always present");
+            Trace.Assert(beforeAndAfter.HasFlag(beforeAndAfter), "HasFlag() doesn't like multi-flags");
+            var beforeAfterDuring = beforeAndAfter | MyFlags.During;
+            Trace.Assert(beforeAfterDuring.HasFlag(beforeAndAfter), "HasFlag() cannot check for multiple flags");
+            Trace.Assert(!beforeAndAfter.HasFlag(beforeAfterDuring), "HasFlag() cannot handle multi flags");
         }
     }
 }
