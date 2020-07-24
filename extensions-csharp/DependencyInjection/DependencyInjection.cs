ddf18947ae0824579204b953183b8b583b20bebf
Peter Leong <leongp@wello.ca>
2020-07-24 16:14:12 -0700

    DI experiment

--- extensions-csharp/DependencyInjection/DependencyInjection.cs
+++ extensions-csharp/DependencyInjection/DependencyInjection.cs
@@ -12,17 +12,21 @@ namespace extensions_csharp.DependencyInjection
             var injector = new ServiceCollection()
                 .AddSingleton<HelloWorld>()
                 .AddSingleton<HelloYellow>()
+                .AddSingleton<FooImpl>()
                 .AddSingleton<IHello, HelloWorld>()
                 .AddSingleton<HelloFactory>()
                 .BuildServiceProvider();
 
-            var hello = injector.GetService<IHello>();
+            var hello = injector.GetRequiredService<IHello>();
             Trace.Assert(hello.Hello() == "World");
 
-            var yellow = injector.GetService<HelloFactory>()
+            var yellow = injector.GetRequiredService<HelloFactory>()
                 .Create(HelloFlavour.Yellow);
             Trace.Assert(yellow.Hello() == "Yellow");
 
+            // NO... is not clever about implemented interfaces... which is prob good
+            // var foo = injector.GetRequiredService<IFoo>();
+            // Trace.Assert(foo is FooImpl);
         }
     }
 }
