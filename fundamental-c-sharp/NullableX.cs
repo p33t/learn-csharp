2085073492ce99a5a0cb35a13efa7f3141b67eb0
Peter Leong <leongp@wello.ca>
2020-07-27 16:18:21 -0700

    Ignore nullable cast with !

--- fundamental-c-sharp/NullableX.cs
+++ fundamental-c-sharp/NullableX.cs
@@ -10,12 +10,15 @@ namespace fundamental_c_sharp
             public bool SomeFlag { get; } = false;
 
             public bool SomeMethod() => false;
+
+            public int? SomeInt { get; } = 33;
         }
         public static void Demo()
         {
             Console.WriteLine("Nullable ...");
             MyClass? opt = null;
             bool? optBool = null;
+            int? optInt = null;
             
             Trace.Assert(opt is null);
             Trace.Assert(optBool is null);
@@ -27,12 +30,16 @@ namespace fundamental_c_sharp
             Trace.Assert(opt?.SomeFlag is false == false);
             Trace.Assert(opt?.SomeMethod() is true == false);
             Trace.Assert(opt?.SomeMethod() is false == false);
+
+            Trace.Assert(optInt == null);
             
             opt = new MyClass();
             optBool = false;
+            optInt = 99;
             
             Trace.Assert(opt is MyClass);
             Trace.Assert(optBool is bool);
+            Trace.Assert(optInt is int);
 
             Trace.Assert(opt?.SomeFlag != true);
             Trace.Assert(opt?.SomeFlag == false);
@@ -41,6 +48,9 @@ namespace fundamental_c_sharp
             Trace.Assert(opt?.SomeFlag is false);
             Trace.Assert(opt?.SomeMethod() is true == false);
             Trace.Assert(opt?.SomeMethod() is false);
+
+            Trace.Assert(optInt! == 99);
+            Trace.Assert(opt!.SomeInt! == 33);
         }
     }
 }
