189df4825678aa513885f5939aa0e8ccacd9058f
Peter Leong <leongp@wello.ca>
2021-04-13 16:00:41 -0700

    DispatchProxy

---
+++ fundamental-c-sharp/DispatchProxying.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace fundamental_c_sharp
+{
+    public static class DispatchProxying
+    {
+        public interface IHello
+        {
+            string Hello();
+        }
+
+        public class HelloImpl : IHello
+        {
+            private readonly string _world;
+
+            public HelloImpl(string world)
+            {
+                _world = world;
+            }
+
+            public string Hello()
+            {
+                return _world;
+            }
+        }
+
+        public class HelloService : DispatchProxy
+        {
+            private IHello? _hello;
+
+            public void Init(string world)
+            {
+                _hello = new HelloImpl(world);
+            }
+
+            protected override object Invoke(MethodInfo targetMethod, object[] args)
+            {
+                Console.WriteLine($"Invoking {targetMethod.Name}()");
+                if (targetMethod.Name == nameof(IHello.Hello) && targetMethod.DeclaringType == typeof(IHello))
+                    return _hello!.Hello();
+
+                throw new ArgumentException($"Unknown method {targetMethod.Name}", nameof(targetMethod));
+            }
+        }
+
+        public static void Demo()
+        {
+            Console.WriteLine("Dispatch Proxying ============================");
+
+            var hello = DispatchProxy.Create<IHello, HelloService>();
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            ((HelloService)hello).Init("World!");
+            Debug.Assert(hello.Hello() == "World!");
+        }
+    }
+}
--- fundamental-c-sharp/Program.cs
+++ fundamental-c-sharp/Program.cs
@@ -8,33 +8,34 @@ namespace fundamental_c_sharp
     {
         static async Task Main(string[] args)
         {
-            Util.WriteLn("Hello C#");
-            Generics.Demo();
-            Time.Timestamps();
-            ParameterPassing.Demo();
-            Constructors.Demo();
-            Polymorphism.Demo();
-            Functions.Demo();
-            Delegates.Demo();
-            Reflection.DispatchProxy();
-            Reflection.PropertyInvocation();
-            ExtensionMethod.ExtensionMethod.Demo();
-            // Too slow... TaskAsync.Demo().Wait();
-            Enums.Demo();
-            NullableX.Demo();
-            ValueTuples.Demo();
-            PartialClass.PartialClass.Demo();
-            Attributes.Attributes.Demo();
-            DateTimes.Demo();
-            DateTimeOffsets.Demo();
-            Casting.Demo();
-            Equality.Demo();
-            Expressions.Demo();
-            Switches.Demo();
-            await TaskAsync.Demo();
-            TaskWait.Demo();
+            // Util.WriteLn("Hello C#");
+            // Generics.Demo();
+            // Time.Timestamps();
+            // ParameterPassing.Demo();
+            // Constructors.Demo();
+            // Polymorphism.Demo();
+            // Functions.Demo();
+            // Delegates.Demo();
+            // Reflection.DispatchProxy();
+            // Reflection.PropertyInvocation();
+            // ExtensionMethod.ExtensionMethod.Demo();
+            // // Too slow... TaskAsync.Demo().Wait();
+            // Enums.Demo();
+            // NullableX.Demo();
+            // ValueTuples.Demo();
+            // PartialClass.PartialClass.Demo();
+            // Attributes.Attributes.Demo();
+            // DateTimes.Demo();
+            // DateTimeOffsets.Demo();
+            // Casting.Demo();
+            // Equality.Demo();
+            // Expressions.Demo();
+            // Switches.Demo();
+            // await TaskAsync.Demo();
+            // TaskWait.Demo();
             await TaskExceptions.Demo();
-            ClassInitialization.Demo();
+            // ClassInitialization.Demo();
+            DispatchProxying.Demo();
 
             Console.WriteLine("Done.");
         }
