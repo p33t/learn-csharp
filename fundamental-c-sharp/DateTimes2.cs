aec431ed313fd407101c9982bd817440ffeb18ec
p33t <1179301+p33t@users.noreply.github.com>
2021-08-31 16:25:16 -0700

    Write notes and illustrate DateTime practices

--- fundamental-c-sharp/DateTimes2.cs
+++ fundamental-c-sharp/DateTimes2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace fundamental_c_sharp
 {
@@ -9,20 +10,25 @@ namespace fundamental_c_sharp
             Console.WriteLine("DateTimes2 ========================================================");
             var utcCalendarDate = new DateTime(2021, 9, 1, 0, 0, 0, DateTimeKind.Utc);
             var localCalendarDate = new DateTime(2021, 9, 1, 0, 0, 0, DateTimeKind.Local);
+            var unspecifiedCalendarDate = new DateTime(2021, 9, 1, 0, 0, 0, DateTimeKind.Unspecified);
+            
+            // DateTime has an extra component 'kind' which is independent of 'ticks'
+            // Only 'ticks' are used for comparison
+            Debug.Assert(utcCalendarDate.Ticks == localCalendarDate.Ticks);
+            Debug.Assert(utcCalendarDate == localCalendarDate);
+            Debug.Assert(utcCalendarDate.Equals(localCalendarDate));
 
-            var utcNow = DateTime.UtcNow;
-
-            var utcCalendarCurrentTimeOfDay = utcCalendarDate.Add(utcNow.TimeOfDay);
-            var localCalendarCurrentTimeOfDay = localCalendarDate.Add(utcNow.TimeOfDay);
-
-            Console.WriteLine($"Name\t\t\t\tTime\t\t\t\t\tTicks");
-            Console.WriteLine($"{nameof(utcCalendarDate)}\t\t\t{utcCalendarDate:O}\t\t{utcCalendarDate.Ticks}");
-            Console.WriteLine($"{nameof(utcCalendarCurrentTimeOfDay)}\t{utcCalendarCurrentTimeOfDay:O}\t\t{utcCalendarCurrentTimeOfDay.Ticks}");
-            Console.WriteLine($"{nameof(localCalendarDate)}\t\t{localCalendarDate:O}\t{localCalendarDate.Ticks}");
-            Console.WriteLine($"{nameof(localCalendarCurrentTimeOfDay)}\t{localCalendarCurrentTimeOfDay:O}\t{localCalendarCurrentTimeOfDay.Ticks}");
-
-            Console.WriteLine($"Calendar dates equal? {utcCalendarDate == localCalendarDate}");
-            Console.WriteLine($"Calendar dates with times of day equal? {utcCalendarCurrentTimeOfDay == localCalendarCurrentTimeOfDay}");            
+            // Rendering an ISO 8601 string depends on the 'kind' component
+            // So 'equal' DateTime values output different ISO 8601 values (ouch!)
+            Debug.Assert("2021-09-01T00:00:00.0000000Z" == utcCalendarDate.ToString("O"));
+            Debug.Assert("2021-09-01T00:00:00.0000000-07:00" == localCalendarDate.ToString("O"));
+            Debug.Assert("2021-09-01T00:00:00.0000000" == unspecifiedCalendarDate.ToString("O"));
+            
+            // General practice is to assume UTC values are used exclusively on the server
+            // (ignoring the 'kind' which is commonly 'unspecified' but occasionally 'utc')
+            // To obtain an ISO 8601 string in this scenario entails assigning the kind before using ToString("O")
+            var iso8601 = DateTime.SpecifyKind(unspecifiedCalendarDate, DateTimeKind.Utc).ToString("O");
+            Debug.Assert("2021-09-01T00:00:00.0000000Z" == iso8601);
         }
     }
 }
