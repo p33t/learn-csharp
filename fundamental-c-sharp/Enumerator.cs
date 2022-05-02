f50330672d9e6dc30670717d0948b7120cb24337
p33t <1179301+p33t@users.noreply.github.com>
2022-05-01 22:12:05 -0700

    Fundamental IEnumerator

---
+++ fundamental-c-sharp/Enumerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Transactions;
+
+namespace fundamental_c_sharp
+{
+    public static class Enumerators
+    {
+        private static IEnumerator<string> GenerateStrings()
+        {
+            yield return "one";
+            yield return "two";
+            yield return "three";
+        }
+        
+        public static void Demo()
+        {
+            Console.WriteLine("Enumerators ===========================");
+
+            var actual = "";
+            for (var ss = GenerateStrings(); ss.MoveNext();)
+            {
+                actual += "." + ss.Current;
+            }
+            Trace.Assert(actual.Equals(".one.two.three"));
+        }
+    }
+}
