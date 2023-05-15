447ce3720ccf40f7741752d889c313e5f9748d9f
p33t <1179301+p33t@users.noreply.github.com>
2023-05-15 14:12:08 -0700

    [config] Simple configuration loading from JSON

--- configuration/Program.cs
+++ configuration/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Hello, World!");
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json") //, optional: true, reloadOnChange: true)
+    // .AddEnvironmentVariables()
+    // .AddCommandLine(args, new Dictionary<string, string>
+    // {
+    //     {"--log-level", "Serilog__MinimumLevel"}
+    // })
+    .Build();
+
+// Basic JSON   =============================================
+Trace.Assert("Value1" == configuration["Key1"]);
+
+var section1 = configuration.GetSection("Section1");
+Trace.Assert("Value11" == section1["Key11"]);
+
+
+
+
+Console.WriteLine("Tests have succeeded.");
