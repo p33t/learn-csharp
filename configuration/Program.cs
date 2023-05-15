9feb8628971e013989d840d13a2dab210bdabd49
p33t <1179301+p33t@users.noreply.github.com>
2023-05-15 14:15:28 -0700

    [config] Via environment var

--- configuration/Program.cs
+++ configuration/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Configuration;
 
 IConfiguration configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json") //, optional: true, reloadOnChange: true)
-    // .AddEnvironmentVariables()
+    .AddEnvironmentVariables()
     // .AddCommandLine(args, new Dictionary<string, string>
     // {
     //     {"--log-level", "Serilog__MinimumLevel"}
@@ -17,6 +17,11 @@ var section1 = configuration.GetSection("Section1");
 Trace.Assert("Value11" == section1["Key11"]);
 
 
+// Supplied by environment var ==============================
+// This env var must be present (can supply in JB Rider run config)
+// Section2__Key21=Value21
+var section2 = configuration.GetSection("Section2");
+Trace.Assert("Value21" == section2["Key21"]);
 
 
 Console.WriteLine("Tests have succeeded.");
