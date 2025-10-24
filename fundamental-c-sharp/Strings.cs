bfae3157e16232c16d4557a1c40795a58a8f0956
p33t <1179301+p33t@users.noreply.github.com>
2025-10-24 15:07:08 -0700

    [Fundamentals] string.Join

--- fundamental-c-sharp/Strings.cs
+++ fundamental-c-sharp/Strings.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace fundamental_c_sharp;
 
@@ -14,5 +15,8 @@ public class Strings
         var bytes = BitConverter.GetBytes(45612390);
         var hex = BitConverter.ToString(bytes);
         Trace.Assert("66-FD-B7-02".Equals(hex));
+
+        var actualJoin = string.Join("-", new[] { "a", null, "b", "c", null }.Where(s => s != null));
+        Trace.Assert("a-b-c".Equals(actualJoin));
     }
 }
